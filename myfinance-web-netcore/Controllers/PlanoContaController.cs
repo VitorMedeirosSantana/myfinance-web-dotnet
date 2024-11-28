af0a02af746a9e00d3e7faf562e761ed0e42366b
Vitor <lennon_vms@hotmail.com>
2024-11-27 22:32:54 -0300

    Botões e criação da tela de cadastro

--- myfinance-web-netcore/Controllers/PlanoContaController.cs
+++ myfinance-web-netcore/Controllers/PlanoContaController.cs
@@ -6,19 +6,31 @@ using System.Diagnostics;
 
 namespace myfinance_web_netcore.Controllers
 {
+
+    [Route("[Controller]")]
     public class PlanoContaController : Controller
     {
         private readonly IPlanoContaService _planoContaService;
 
-        public PlanoContaController(IPlanoContaService planoContaService) 
-        { 
+        public PlanoContaController(IPlanoContaService planoContaService)
+        {
             _planoContaService = planoContaService;
         }
 
-
+        [Route("Index")]
+        
         public IActionResult Index()
         {
             ViewBag.Lista = _planoContaService.ListarRegistros();
+            return View();
+        }
+
+        [HttpGet]
+        [HttpPost]
+        [Route("Cadastro")]
+
+        public IActionResult Cadastro(PlanoContaModel model)
+        {
             return View();
         }
 
